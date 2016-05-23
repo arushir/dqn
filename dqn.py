d7defa9b382520c2820d97fb7d937c23725b9b14
Arushi Raghuvanshi <arushi.raghu@gmail.com>
2016-05-23 15:43:46 -0700

    cleaned up dqn file

--- dqn.py
+++ dqn.py
@@ -22,18 +22,35 @@ class DQN:
     print "model initialized"
 
   def select_action(self, observation):
+    """
+    Selects the next action to take based on the current state and learned Q.
+
+    Args:
+      observation: the current state
+
+    """
+
     if random.random() < self.epsilon: 
       # with epsilon probability select a random action 
       action = np.random.randint(0, self.num_actions)
     else:
       # select the action a which maximizes the Q value
-      # TODO: check if we have to flatten the observation
-      # TODO: what is the difference between Q* and Q?
       q_values = self.model.predict(observation)
       action = np.argmax(q_values)
     return action
 
   def update_state(self, action, observation, new_observation, reward, done):
+    """
+    Stores the most recent action in the replay memory.
+
+    Args: 
+      action: the action taken 
+      observation: the state before the action was taken
+      new_observation: the state after the action is taken
+      reward: the reward from the action
+      done: a boolean for when the episode has terminated 
+
+    """
     transition = {'action': action,
                   'observation': observation,
                   'new_observation': new_observation,
@@ -42,6 +59,10 @@ class DQN:
     self.memory.append(transition)
 
   def get_random_mini_batch(self):
+    """
+    Gets a random sample of transitions from the replay memory.
+
+    """
     rand_idxs = random.sample(xrange(len(self.memory)), len(self.memory))
     mini_batch = []
     for idx in rand_idxs:
@@ -49,11 +70,10 @@ class DQN:
     return mini_batch
 
   def train_step(self):
-    """Learn on the mini batch"""
-    #self.model.train_step(observation)
+    """
+    Updates the model based on the mini batch
 
-    print len(self.memory)
-    print self.mini_batch_size
+    """
     if len(self.memory) > self.mini_batch_size:
       mini_batch = self.get_random_mini_batch()
 
@@ -63,10 +83,10 @@ class DQN:
       for sample in mini_batch:
         y_j = np.zeros(self.num_actions)
         y_j += sample['reward']
+
         # for nonterminals, add gamma*max_a(Q(phi_{j+1})) term
         if not sample['is_done']:
           q_values = self.model.predict(sample['new_observation'])
-          # TODO: should this be max or argmax?
           action = np.max(q_values)
           y_j += self.gamma*action
 
@@ -76,8 +96,6 @@ class DQN:
 
       Xs = np.array(Xs)
       ys = np.array(ys)
-      print "Xs: ", Xs
-      print "ys: ", ys
 
       self.model.train_step(Xs, ys)
 
